146a4a68ba7cd59d51ff9bc5a9790a841a1c6669
jbboncay17 <jbboncay1@gmail.com>
2026-04-29 20:15:06 +0800

    final changes and added variables

--- integers_square_and_cube.py
+++ integers_square_and_cube.py
@@ -5,3 +5,12 @@ class SquareAndCube:
     def transform(self):
         with open(self.file_name, 'r') as input_file:
             number_list = list(map(int, input_file.read().split()))
+
+            even_number_squares = [number ** 2 for number in number_list if number % 2 == 0]
+            odd_number_cubes = [number ** 3 for number in number_list if number % 2 != 0]
+
+            with open("squares.txt", 'w') as squares_file:
+                squares_file.write(" ".join(map(str, even_number_squares)))
+
+            with open("cubes.txt", 'w') as cubes_file:
+                cubes_file.write(" ".join(map(str, odd_number_cubes)))
