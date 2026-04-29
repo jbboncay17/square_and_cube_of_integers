c7682cd648748bae5430078b33d1e9c8ba55b02e
jbboncay17 <jbboncay1@gmail.com>
2026-04-29 20:24:57 +0800

    final import

---
+++ run_integers_square_and_cube.py
@@ -0,0 +1,6 @@
+from integers_square_and_cube import SquareAndCube
+
+integers_square_and_cube = SquareAndCube("integers.txt")
+integers_square_and_cube.transform()
+
+print("Squares and Cubes files created successfully.")
